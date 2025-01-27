a0a3f36b11e8e28b91687823c2a0fadbaef76c01
Hugo Granberg <hugo.granberg@edu.lycksele.se>
2025-01-27 13:01:32 +0100

    tjena jocke

--- chuck norris/Program.cs
+++ chuck norris/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http;
+
+class Program{
+static async void Main(){
+using(HttpClient goblin = new HttpClient()){
+goblin.BaseAddress = new Uri("https://api.chucknorris.io");
+try{
+  HttpResponseMessage response = await goblin.GetAsync("jokes/random");
+  response.EnsureSuccessStatusCode();
+  string responeseBody = await response.Content.ReadAsStringAsync();
+  Console.WriteLine(responeseBody);
+}
+catch(HttpRequestException e){
+Console.WriteLine(e.Message);
+}
+}
+}
+}
+
